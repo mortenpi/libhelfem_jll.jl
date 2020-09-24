594981a76c424fa9cab3b60cea18d51293aa01bd
Morten Piibeleht <morten.piibeleht@gmail.com>
2020-09-24 14:26:54 +1200

    Make wrapper cross-compile

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,9 +1,10 @@
 # Modes:
 #
 # * julia --project build_tarballs.jl --verbose --devdir=dev --deploy=local
-#   Non-interactive local build
+#   Non-interactive local build. Can also add --debug to drop into the debug shell on
+#   failure.
 #
-# * julia --project --color=yes build_tarballs.jl --debug --verbose --deploy="mortenpi/libhelfem_jll.jl"
+# * julia --project --color=yes build_tarballs.jl --verbose --deploy="mortenpi/libhelfem_jll.jl"
 #   Deploy to GitHub. This requires ENV["GITHUB_TOKEN"] to be set up for authentication.
 #
 # * julia --project -i build_tarballs.jl
@@ -42,23 +43,60 @@ if [ -d "${WORKSPACE}/srcdir/HelFEM-$(:version)" ]; then
     mv "${WORKSPACE}/srcdir/HelFEM-$(:version)" "${WORKSPACE}/srcdir/HelFEM"
 fi
 cp -v ${WORKSPACE}/srcdir/CMake.system ${WORKSPACE}/srcdir/HelFEM/CMake.system
+
+# Set up some platform specific CMake configuration. This is more or less borrowed from
+# the Armadillo build_tarballs.jl script:
+#   https://github.com/JuliaPackaging/Yggdrasil/blob/48d7a89b4aa46b1a8c91269bb138a660f4ee4ece/A/armadillo/build_tarballs.jl#L23-L52
+#
+# We need to manually set up OpenBLAS because FindOpenBLAS.cmake does not work with BB:
+if [[ "${nbits}" == 64 ]] && [[ "${target}" != aarch64* ]]; then
+    OPENBLAS="${libdir}/libopenblas64_.${dlext}"
+else
+    OPENBLAS="${libdir}/libopenblas.${dlext}"
+fi
+
 # Compile libhelfem as a static library
 cd ${WORKSPACE}/srcdir/HelFEM
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DUSE_OPENMP=OFF -B build/ -S .
+cmake \
+    -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+    -DUSE_OPENMP=OFF \
+    -B build/ -S .
 make -C build/ -j${nproc} helfem
 make -C build/ install
+# Copy the HelFEM license
+install_license LICENSE
+
 # Compile the CxxWrap wrapper as a shared library
 cd ${WORKSPACE}/srcdir
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -B build/ -S .
+cmake \
+    -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release \
+    -DBLAS_LIBRARIES=${OPENBLAS} \
+    -DJulia_PREFIX=$prefix -DCMAKE_FIND_ROOT_PATH=$prefix -DJlCxx_DIR=$prefix/lib/cmake/JlCxx \
+    -B build/ -S .
 make -C build/ -j${nproc}
 make -C build/ install
 """
 script = replace(script, raw"$(:version)" => "$version")
 
-#platforms = supported_platforms()
+# These are the platforms the libcxxwrap_julia_jll is built on.
 platforms = [
-    Linux(:i686, libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
-    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    # x86_64-linux-gnu-cxx11
+    Linux(:x86_64; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    # i686-linux-gnu-cxx11
+    Linux(:i686; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    # armv7l-linux-gnueabihf-cxx11
+    Linux(:armv7l; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    # aarch64-linux-gnu-cxx11
+    Linux(:aarch64; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    # x86_64-apple-darwin14-cxx11
+    MacOS(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    # x86_64-w64-mingw32-cxx11
+    Windows(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    # i686-w64-mingw32-cxx11
+    Windows(:i686; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    # x86_64-unknown-freebsd11.1-cxx11
+    FreeBSD(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
 ]
 
 products = [
